3ae49d86c6f4fdde74e678557d90b08a1c8456d3
Bruno Moraes <brunomoraes.dev@gmail.com>
2025-01-14 22:01:15 -0300

    140125.2201

--- engine.js
+++ engine.js
@@ -1,13 +1,55 @@
 const elementoCaixaDoJogo = document.getElementById('caixaDoJogo');
-const elementoTabuleiro = document.getElementById('tabuleiro');
-const elementoBola = document.getElementById('bola');
-const elementoRaqueteEsquerda = document.getElementById('raqueteEsquerda');
-const elementoRaqueteDireita = document.getElementById('raqueteDireita');
 
 // Elementos da interface
 const elementoInterface = document.getElementById('interface');
 const elementoPontuacao = document.getElementById('pontuacao');
 const elementoNivel = document.getElementById('nivel');
 const elementoVidas = document.getElementById('vidas'); 
- 
-dont
+
+// Elementos do tabuleiro
+const elementoTabuleiro = document.getElementById('tabuleiro');
+const elementoBola = document.getElementById('bola');
+const elementoraquete = document.getElementById('raquete');
+const elementoBotao = document.getElementById('botao');
+
+let larguraDaTela = window.innerWidth;
+let alturaDaTela = window.innerHeight;
+
+let larguraElementoCaixaDoJogo;
+let alturaElementoCaixaDoJogo;
+
+
+function carregar_elementos () {
+    larguraElementoCaixaDoJogo = Math.floor((larguraDaTela*90)/100);
+    alturaElementoCaixaDoJogo = Math.floor((alturaDaTela*70)/100);
+
+    elementoCaixaDoJogo.style.width = `${larguraElementoCaixaDoJogo}px`;
+    elementoCaixaDoJogo.style.height = `${alturaElementoCaixaDoJogo}px`;
+
+    elementoBola.style.width = `${(larguraElementoCaixaDoJogo*2)/100}px`;
+    elementoBola.style.height = `${(larguraElementoCaixaDoJogo*2)/100}px`;
+
+    elementoraquete.style.height = `${(larguraElementoCaixaDoJogo*2)/100}px`;
+    elementoraquete.style.width = `${(larguraElementoCaixaDoJogo*15)/100}px`;
+    
+}
+
+function iniciar_jogo () {
+    botao.style.display = "none";
+}
+
+function carregar_jogo () {
+    carregar_elementos();
+
+}
+
+carregar_jogo();
+
+// Responsividade
+/*
+window.addEventListener('resize', () => {
+    larguraDaTela = window.innerWidth;
+    alturaDaTela = window.innerHeight;
+    carregar_jogo();
+});
+*/
