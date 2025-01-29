f5d5e1420c2985e45f017b547e4022da9b69925f
Bruno Moraes <brunomoraes.dev@gmail.com>
2025-01-28 21:23:07 -0300

    280125.2122 ajustei o css, mais especificamente a largura/altura dos elementos bola, raquete e tabuleiro

--- engine.js
+++ engine.js
@@ -6,11 +6,10 @@ function obterDimensoesDaTela() {
         'Dimensões da Tela\n' +
         '\tLargura:\t' + largura +
         '\n\tAltura:\t\t' + altura
-
     )
+
     return { largura, altura };
 }
-
 
 const ALTURA_REFERENCIA = 914;
 const FONT_REFERENCIA = 16;
@@ -18,30 +17,9 @@ function atualizarTamanhoDaFonte(altura) {
     let tamanhoDaFonte = Math.floor((FONT_REFERENCIA * altura) / ALTURA_REFERENCIA);
     document.documentElement.style.setProperty('--font-size', tamanhoDaFonte + 'px');
 
-    console.log('Fonte: ' + tamanhoDaFonte);
+    console.log('Tamanho da Fonte: ' + tamanhoDaFonte);
 }
-
-
-function carregarLayout() {
-    // Destructuring para pegar a altura
-    const { altura } = obterDimensoesDaTela();
-    atualizarTamanhoDaFonte(altura);
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    carregarLayout();  // Garante que o layout seja ajustado no carregamento
-});
-
-let debounce;
-function aoRedimensionar() {
-    clearTimeout(debounce);
-    debounce = setTimeout(function () {
-        carregarLayout(); 
-    }, 16); 
-}
-window.addEventListener('resize', aoRedimensionar);
-
-/* ************* */
+/* ----------------------- */
 
 const elementoTabuleiro = document.getElementById('tabuleiro');
 const elementoRaquete = document.getElementById('raquete');
@@ -50,24 +28,47 @@ const elementoBola = document.getElementById('bola');
 function obterDimensoesDosElementos(elemento) {
     return {
         largura: elemento.offsetWidth,
-        altura: elemento.offsetHeight
+        altura: elemento.offsetHeight,
     };
 }
-const dimensoesTabuleiro = obterDimensoesDosElementos(elementoTabuleiro);
-const dimensoesRaquete = obterDimensoesDosElementos(elementoRaquete);
-const dimensoesBola = obterDimensoesDosElementos(elementoBola);
 
-console.log(`Tabuleiro\n\tLargura: \t${dimensoesTabuleiro.largura}px \n\tAltura: \t${dimensoesTabuleiro.altura}px`);
-console.log(`Raquete\n\tLargura: \t${dimensoesRaquete.largura}px \n\tAltura: \t${dimensoesRaquete.altura}px`);
-console.log(`Bola\n\tLargura: \t${dimensoesBola.largura}px \n\tAltura: \t${dimensoesBola.altura}px`);
+function dimensoesDosElementos() {
+    const dimensoesTabuleiro = obterDimensoesDosElementos(elementoTabuleiro);
+    const dimensoesRaquete = obterDimensoesDosElementos(elementoRaquete);
+    const dimensoesBola = obterDimensoesDosElementos(elementoBola);
 
+    console.log(`Tabuleiro\n\tLargura: \t${dimensoesTabuleiro.largura}px \n\tAltura: \t${dimensoesTabuleiro.altura}px`);
+    console.log(`Raquete\n\tLargura: \t${dimensoesRaquete.largura}px \n\tAltura: \t${dimensoesRaquete.altura}px`);
+    console.log(`Bola\n\tLargura: \t${dimensoesBola.largura}px \n\tAltura: \t${dimensoesBola.altura}px`);
+}
+
+/* --------------------------------- */
+function carregarLayout() {
+    const { altura } = obterDimensoesDaTela();
+    atualizarTamanhoDaFonte(altura);
+    dimensoesDosElementos();
+    console.log('-----------------------');
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    carregarLayout();
+});
+
+let debounce;
+function aoRedimensionar() {
+    clearTimeout(debounce);
+    debounce = setTimeout(function () {
+        carregarLayout();
+    }, 16);
+}
+window.addEventListener('resize', aoRedimensionar);
 
 function iniciar_jogo() {
     controlarBotao();
 }
 
 function controlarBotao() {
-    const elementoBotao = document.getElementById('botao');
-    elementoBotao.style.display = 'none';
-
+    carregarLayout();
 }
+
+
